67a25df926b1bde1ad9d275e7239fcd3a060d73d
Seungje-Lee <tmdwp54977@gmail.com>
2025-09-26 20:18:20 +0900

    Commets added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import QueryInput from "@/components/todo-query-input";
+import QueryInput from "@/components/query-input";
 
 import React, { useRef, useState } from "react";
 import {
@@ -16,57 +16,55 @@ import {
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
 
-// 화면 너비 계산
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
-// console.log(screenHeight);
-// 스크린 전체 높이 896
 
-// 월 및 요일 이름
 const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  "1월",
+  "2월",
+  "3월",
+  "4월",
+  "5월",
+  "6월",
+  "7월",
+  "8월",
+  "9월",
+  "10월",
+  "11월",
+  "12월",
 ];
+const dayNames = ["일", "월", "화", "수", "목", "금", "토"];
 
-const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-// 기준 월 인덱스
+// 기준 월(오늘 날짜가 속한 달)의 index
 const INITIAL_INDEX = 1000;
 
-// 달력 컴포넌트
 export default function Calendar() {
   const [currentMonthIndex, setCurrentMonthIndex] = useState(INITIAL_INDEX);
-  const insets = useSafeAreaInsets();
-  //const usableHeight = screenHeight - insets.top - insets.bottom;
-  const safeAreaHeight = insets.top;
-  // console.log("실제 렌더링 가능한 높이:", usableHeight, insets.top);
 
+  // containerHeight: 최상단 SafeAreaView의 디스플레이 세로 픽셀 값 / _layout.tsx의 <Tabs> 높이 제외
+  const [containerHeight, setContainerHeight] = useState(0);
+  // 상단 notch 부분 높이
+  const safeAreaHeight = useSafeAreaInsets().top;
+  //
+  const etcHeightPixels = 187;
+
+  // SafeAreaView 구성 요소가 렌더링되고 React Native 엔진에 의해 레이아웃이 계산된 후 정확한 높이를 logging
   const handleLayout = (event: LayoutChangeEvent) => {
     const { height } = event.nativeEvent.layout;
-    // setContainerHeight(height);
-    console.log("Container height:", height);
+    setContainerHeight(height);
+    console.log("containerHeight", height);
   };
 
   const baseDate = new Date();
 
-  // 특정 인덱스로부터 날짜 생성
+  // 특정 인덱스에 대응되는 날짜 생성
   const getDateFromIndex = (index: number) => {
     const date = new Date(baseDate);
     date.setMonth(date.getMonth() + (index - INITIAL_INDEX));
     return date;
   };
 
-  // 날짜 관련 유틸 함수
+  // 날짜 관련 함수
   const getDaysInMonth = (year: number, month: number) =>
     new Date(year, month + 1, 0).getDate();
 
@@ -75,6 +73,16 @@ export default function Calendar() {
 
   const getDaysInPrevMonth = (year: number, month: number) =>
     new Date(year, month, 0).getDate();
+
+  // 스와이프로 달력 월 변경 시 index 업데이트
+  const onViewableItemsChanged = useRef(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      if (viewableItems.length > 0) {
+        const index = viewableItems[0].index ?? INITIAL_INDEX;
+        setCurrentMonthIndex(index);
+      }
+    }
+  ).current;
 
   // 달력 렌더링
   const renderCalendar = (date: Date) => {
@@ -88,14 +96,18 @@ export default function Calendar() {
 
     const days = [];
 
-    // 이전 달 날짜
+    // 이전 달의 날짜 component 추가
     for (let i = firstDay; i > 0; i--) {
       days.push(
         <View
           key={`prev-${i}`}
           style={[
             styles.dayCell,
-            { height: (screenHeight - 83 - 240.5 - safeAreaHeight) / 6 },
+            {
+              // 화면 세로 픽셀 - 상단 노치 - 기타 padding, margin 등으로
+              // 렌더링할 수 없는 부분 제외하고 날짜 칸의 높이가 최대가 되도록 계산
+              height: (containerHeight - safeAreaHeight - etcHeightPixels) / 6,
+            },
           ]}
         >
           <Text style={styles.dayTextInactive}>{daysInPrevMonth - i + 1}</Text>
@@ -103,7 +115,7 @@ export default function Calendar() {
       );
     }
 
-    // 현재 달 날짜
+    // 현재 달 날짜 component 추가
     for (let i = 1; i <= daysInMonth; i++) {
       const today = new Date();
       const isToday =
@@ -116,34 +128,31 @@ export default function Calendar() {
           key={`current-${i}`}
           style={[
             styles.dayCell,
-            { height: (screenHeight - 83 - 240.5 - safeAreaHeight) / 6 },
-            ////          896.     하단 tab 기타 값   노치
+            {
+              height: (containerHeight - safeAreaHeight - etcHeightPixels) / 6,
+            },
             isToday && styles.todayCell,
           ]}
         >
           <Text style={[styles.dayText, isToday && styles.todayText]}>{i}</Text>
-
           <View style={styles.todoContainer}>
             <Text key={1} style={styles.todoText}>
-              todos
+              todo
             </Text>
-
             <Text key={2} style={styles.todoText}>
-              todos
+              todo
             </Text>
             <Text key={3} style={styles.todoText}>
-              todos
-            </Text>
-            <Text key={4} style={styles.todoText}>
-              todos
+              todo
             </Text>
           </View>
         </View>
       );
     }
 
-    // 다음 달 날짜
+    // 다음 달의 날짜 component 추가
     if (lastDay !== 6) {
+      // 이번 달이 토요일로 끝나지 않으면
       const trailingDays = 6 - lastDay;
       for (let i = 1; i <= trailingDays; i++) {
         days.push(
@@ -151,7 +160,10 @@ export default function Calendar() {
             key={`next-${i}`}
             style={[
               styles.dayCell,
-              { height: (screenHeight - 83 - 240.5 - safeAreaHeight) / 6 },
+              {
+                height:
+                  (containerHeight - safeAreaHeight - etcHeightPixels) / 6,
+              },
             ]}
           >
             <Text style={styles.dayTextInactive}>{i}</Text>
@@ -160,70 +172,56 @@ export default function Calendar() {
       }
     }
 
+    // 실제 calendar view를 구성하는 부분
     return (
       <View style={styles.card}>
         <View style={styles.header}>
-          {/* <TouchableOpacity
-            onPress={handlePrevious}
-            style={styles.navButton}
-            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }} // Optional: Increases the tappable area
-          >
-            <Text style={styles.navText}>{"<"}</Text>
-          </TouchableOpacity> */}
           <View style={styles.monthYearContainer}>
             <Text style={styles.monthText}>{monthNames[month]}</Text>
-            <Text style={styles.yearText}>{year}</Text>
           </View>
-          {/* <TouchableOpacity
-            onPress={handleNext}
-            style={styles.navButton}
-            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-          >
-            <Text style={styles.navText}>{">"}</Text>
-          </TouchableOpacity> */}
         </View>
-
         <View style={styles.dayNameContainer}>
           {dayNames.map((day) => (
             <View key={day} style={styles.dayNameCell}>
-              <Text style={styles.dayNameText}>{day}</Text>
+              <Text
+                style={[styles.dayNameText, day === "일" && styles.sundayText]}
+              >
+                {day}
+              </Text>
             </View>
           ))}
         </View>
-
         <View style={styles.calendarGrid}>{days}</View>
       </View>
     );
   };
 
-  // 뷰 변경 시 월 인덱스 업데이트
-  const onViewableItemsChanged = useRef(
-    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
-      if (viewableItems.length > 0) {
-        const index = viewableItems[0].index ?? INITIAL_INDEX;
-        setCurrentMonthIndex(index);
-      }
-    }
-  ).current;
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container} onLayout={handleLayout}>
-        {/* 월별 스크롤 가능한 달력 */}
+        {/* 월별 횡 스크롤 가능한 달력 */}
         <FlatList
           data={Array.from({ length: 2000 })}
+          // horizontal: 횡 방향으로의 list 나열
           horizontal
+          // pagingEnabled: 스와이프 시 전/후 월로 전환하는 props
           pagingEnabled
+          // initialScrollIndex: 현재 월 기준으로 초기 달이 보이도록 설정
           initialScrollIndex={INITIAL_INDEX}
+          // getItemLayout: 각 달의 달력 가로 길이는 모두 동일하므로 미리 계산하여 계산량 줄임
           getItemLayout={(_, index) => ({
             length: screenWidth,
             offset: screenWidth * index,
             index,
           })}
           showsHorizontalScrollIndicator={false}
+          // onViewableItemsChanged: 횡 스크롤로 보여야 하는 index가 바뀔 경우 trigger됨
           onViewableItemsChanged={onViewableItemsChanged}
+          // viewabilityConfig: item이 언제 viewable하다고 결정할 지 정하는 부분
+          // 횡스크롤하여 50% 이상 보일 때 'viewable'하다고 설정
           viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
           keyExtractor={(_, index) => index.toString()}
+          // renderItem: index 변화 시 실제 보일 내용을 렌더링하는 부분
           renderItem={({ index }) => {
             const date = getDateFromIndex(index);
             return (
@@ -241,57 +239,47 @@ export default function Calendar() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // 완전히 white로 하면 background가 안 보임
     backgroundColor: "#888",
     alignItems: "center",
     // justifyContent: "center",
     // 5주, 6주에 따른 높이가 다르기 때문에
-    // calendarGrid를 세로 중앙 정렬시키는 justifyContent를 삭제하고
-    // padding??으로 styles.card을 아래로 밀어줌
-    // paddingTop으로 밀어주면 StatusBar를 가림
-    // paddingTop: 30,
-    // marginTop: 50,
+    // calendarGrid를 세로 중앙 정렬시키는 justifyContent를 삭제
   },
   card: {
-    backgroundColor: "#000",
+    flex: 1,
+    backgroundColor: "white",
     // borderRadius: 24,
     // shadowColor: "#000",
     // shadowOffset: { width: 0, height: 4 },
     // shadowOpacity: 0.1,
     // shadowRadius: 10,
     // elevation: 8,
-
-    paddingVertical: 24,
-    paddingHorizontal: 6,
+    // paddingVertical: 12,
+    paddingHorizontal: 16,
+    // paddingHorizontal: 6,
     width: screenWidth,
     maxWidth: 600,
   },
   header: {
     flexDirection: "row",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 24,
-  },
-  navButton: {
-    padding: 8,
-    borderRadius: 9999, // A large value for a circle
-  },
-  navText: {
-    fontSize: 24,
-    color: "#4b5563",
   },
   monthYearContainer: {
     alignItems: "center",
   },
   monthText: {
-    fontSize: 28,
+    fontSize: 24,
+    lineHeight: 24 * 1.2,
     fontWeight: "800",
-    color: "#fff",
+    color: "#000",
   },
   yearText: {
     fontSize: 18,
+    // lineHeight: 상하 추가적인 여백 없이 fontSize와 최대한 동일하게 계산하기 위해
+    lineHeight: 18,
     fontWeight: "500",
-    color: "#fff",
+    color: "#000",
     marginTop: 4,
   },
   dayNameContainer: {
@@ -309,14 +297,16 @@ const styles = StyleSheet.create({
   },
   dayNameText: {
     fontSize: 12,
+    lineHeight: 12,
     fontWeight: "600",
-    color: "#fff",
+    color: "#1E1E1E",
     textTransform: "uppercase",
   },
   calendarGrid: {
     flexDirection: "row",
     flexWrap: "wrap",
-    marginTop: 8,
+    // paddingHorizontal: 16,
+    // marginTop: 8,
   },
   dayCell: {
     width: "14.28%", // 100% / 7 days
@@ -327,16 +317,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 8,
     borderRadius: 9999,
-
-    // added
-    // 여기가 각 날짜의 세로 간격 벌리는 곳
-    // marginVertical: 20,
-    // marginBottom: 60,
   },
   dayText: {
     fontSize: 16,
+    lineHeight: 16,
     fontWeight: "600",
-    color: "#fff",
+    color: "#1E1E1E",
+  },
+  sundayText: {
+    color: "#F0A796",
   },
   todayCell: {
     backgroundColor: "tomato",
@@ -346,17 +335,23 @@ const styles = StyleSheet.create({
   },
   dayTextInactive: {
     fontSize: 16,
+    lineHeight: 16,
     fontWeight: "500",
     color: "#9ca3af",
   },
   todoContainer: {
     marginTop: 4,
     alignItems: "center",
-    zIndex: 3,
+    // zIndex: 3,
     backgroundColor: "#6b7280",
   },
   todoText: {
     fontSize: 15,
+    lineHeight: 15,
     color: "#fff",
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
 });
